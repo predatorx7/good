9b8fbb3146d56fa590c957263075d047a095fef0
predatorx7 <smushaheed@outlook.com>
2024-06-17 03:40:24 +0530

    Add solution for leetcode4

--- cmd/leetcode4/main.go
+++ cmd/leetcode4/main.go
@@ -5,7 +5,13 @@ import (
 	"good/pkg/leetcode4"
 )
 
+func display(nums1 []int, nums2 []int, expected float64) {
+	r := leetcode4.Solve(nums1, nums2)
+	fmt.Printf("expected = %f, result = %f\n", expected, r)
+}
+
 func main() {
-	r := leetcode4.Solve([]int{}, []int{})
-	fmt.Printf("result = %f\n", r)
+	display([]int{1, 2}, []int{3, 4}, 2.5)
+	display([]int{1, 3}, []int{2, 7}, 3.25)
+	display([]int{}, []int{1}, 1)
 }
--- pkg/leetcode4/leetcode4.go
+++ pkg/leetcode4/leetcode4.go
@@ -1,5 +1,31 @@
 package leetcode4
 
+import (
+	"slices"
+)
+
 func Solve(nums1 []int, nums2 []int) float64 {
-	return 2.0
+	n := len(nums1) + len(nums2)
+
+	r := make([]int, n)
+	i := 0
+	for _, e := range nums1 {
+		r[i] = e
+		i++
+	}
+	for _, e := range nums2 {
+		r[i] = e
+		i++
+	}
+
+	// TODO: Check if partial sort is possible
+	slices.Sort(r)
+
+	if n%2 == 0 {
+		center := (n / 2) + 1
+		return (float64(r[center-1]) + float64(r[center-2])) / 2
+	} else {
+		center := (n + 1) / 2
+		return float64(r[center-1])
+	}
 }
