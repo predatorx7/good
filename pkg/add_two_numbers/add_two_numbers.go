5403ead71858cef130b9209b2df16a7c7feba748
predatorx7 <smushaheed@outlook.com>
2024-06-17 00:59:34 +0530

    Update impl of add two numbers

--- pkg/add_two_numbers/add_two_numbers.go
+++ pkg/add_two_numbers/add_two_numbers.go
@@ -10,18 +10,12 @@ type ListNode struct {
 
 func Solve(l1 *ListNode, l2 *ListNode) *ListNode {
 	result := &ListNode{Val: 0}
-	var current *ListNode
+	current := result
 	n1 := l1
 	n2 := l2
 	carry := 0
 
-	for n1 != nil || n2 != nil || carry > 0 {
-		if current == nil {
-			current = result
-		} else {
-			current.Next = &ListNode{Val: 0}
-			current = current.Next
-		}
+	for {
 		if n1 != nil {
 			current.Val += n1.Val
 			n1 = n1.Next
@@ -37,6 +31,12 @@ func Solve(l1 *ListNode, l2 *ListNode) *ListNode {
 		if current.Val > 9 {
 			carry = 1
 			current.Val = current.Val % 10
+		}
+		if n1 != nil || n2 != nil || carry > 0 {
+			current.Next = &ListNode{Val: 0}
+			current = current.Next
+		} else {
+			break
 		}
 	}
 
