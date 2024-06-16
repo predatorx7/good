e3973ac57247c66c212ce5a6260577eb1694099a
predatorx7 <smushaheed@outlook.com>
2024-06-17 01:04:40 +0530

    Improve memory usage golang

--- pkg/add_two_numbers/add_two_numbers.go
+++ pkg/add_two_numbers/add_two_numbers.go
@@ -11,18 +11,16 @@ type ListNode struct {
 func Solve(l1 *ListNode, l2 *ListNode) *ListNode {
 	result := &ListNode{Val: 0}
 	current := result
-	n1 := l1
-	n2 := l2
 	carry := 0
 
 	for {
-		if n1 != nil {
-			current.Val += n1.Val
-			n1 = n1.Next
+		if l1 != nil {
+			current.Val += l1.Val
+			l1 = l1.Next
 		}
-		if n2 != nil {
-			current.Val += n2.Val
-			n2 = n2.Next
+		if l2 != nil {
+			current.Val += l2.Val
+			l2 = l2.Next
 		}
 		if carry > 0 {
 			current.Val += carry
@@ -32,7 +30,7 @@ func Solve(l1 *ListNode, l2 *ListNode) *ListNode {
 			carry = 1
 			current.Val = current.Val % 10
 		}
-		if n1 != nil || n2 != nil || carry > 0 {
+		if l1 != nil || l2 != nil || carry > 0 {
 			current.Next = &ListNode{Val: 0}
 			current = current.Next
 		} else {
