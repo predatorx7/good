03cb75cb0843888eede3b4a91e96e47bdef2949b
predatorx7 <smushaheed@outlook.com>
2024-06-17 02:19:20 +0530

    Add template for leetcode4

---
+++ cmd/leetcode4/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"good/pkg/leetcode4"
+)
+
+func main() {
+	r := leetcode4.Solve([]int{}, []int{})
+	fmt.Printf("result = %f\n", r)
+}
---
+++ pkg/leetcode4/leetcode4.go
@@ -0,0 +1,5 @@
+package leetcode4
+
+func Solve(nums1 []int, nums2 []int) float64 {
+	return 2.0
+}
