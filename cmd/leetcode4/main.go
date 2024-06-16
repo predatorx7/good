cd87c6197df345caaa3032b4c39dc1200f3c820e
predatorx7 <smushaheed@outlook.com>
2024-06-17 04:50:29 +0530

    Update

--- cmd/leetcode4/main.go
+++ cmd/leetcode4/main.go
@@ -12,6 +12,10 @@ func display(nums1 []int, nums2 []int, expected float64) {
 
 func main() {
 	display([]int{1, 2}, []int{3, 4}, 2.5)
-	display([]int{1, 3}, []int{2, 7}, 3.25)
+	display([]int{1, 3}, []int{2, 7}, 2.5)
 	display([]int{}, []int{1}, 1)
+	display([]int{1, 3}, []int{2}, 2)
+	display([]int{2, 3, 4, 5}, []int{1}, 3)
+	display([]int{2, 3, 4, 5, 6, 7}, []int{1}, 4)
+	display([]int{2, 3, 4, 5, 6, 7, 8, 9}, []int{1}, 5)
 }
--- pkg/leetcode4/leetcode4.go
+++ pkg/leetcode4/leetcode4.go
@@ -1,29 +1,23 @@
 package leetcode4
 
-import (
-	"slices"
-)
+import "slices"
 
 func Solve(nums1 []int, nums2 []int) float64 {
 	n := len(nums1) + len(nums2)
 
 	r := make([]int, n)
-	i := 0
-	for _, e := range nums1 {
-		r[i] = e
-		i++
-	}
-	for _, e := range nums2 {
-		r[i] = e
-		i++
+
+	filled := copy(r, nums1)
+
+	for i, e := range nums2 {
+		r[filled+i] = e
 	}
 
-	// TODO: Check if partial sort is possible
 	slices.Sort(r)
 
 	if n%2 == 0 {
 		center := (n / 2) + 1
-		return (float64(r[center-1]) + float64(r[center-2])) / 2
+		return float64(r[center-1]+r[center-2]) / 2
 	} else {
 		center := (n + 1) / 2
 		return float64(r[center-1])
